7dad29efc1b6f2c4d541c889aaa1ec3124222694
Gajanan0530 <143735562+Gajanan0530@users.noreply.github.com>
2026-02-15 13:00:57 +0530

    Add dock panel to plugin with label

--- addons/myPlugin/plugin.gd
+++ addons/myPlugin/plugin.gd
@@ -1,8 +1,17 @@
 @tool
 extends EditorPlugin
 
+var dock
+
 func _enter_tree():
-    print("Plugin enabled")
+	dock = VBoxContainer.new()
+	
+	var label = Label.new()
+	label.text = "My Plugin Dock Panel"
+	dock.add_child(label)
+	
+	add_control_to_dock(DOCK_SLOT_RIGHT_UL, dock)
 
 func _exit_tree():
-    print("Plugin disabled")
+	remove_control_from_docks(dock)
+	dock.queue_free()
